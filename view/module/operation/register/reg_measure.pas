623c0f18af83f984bbfcecfb0a251224b0c8946b
ROD-NOTEBOOK\rod_c <rod.crack3r@gmail.com>
2024-09-10 18:52:26 -0300

    cadastro de unidade de medida

--- view/module/operation/register/reg_measure.pas
+++ view/module/operation/register/reg_measure.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, base_registry, Vcl.Menus, Vcl.Buttons,
-  Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Mask, Vcl.DBCtrls;
+  Vcl.ExtCtrls, Vcl.StdCtrls, Vcl.Mask, Vcl.DBCtrls, ControllerMedida;
 
 type
   TRegMeasure = class(TBaseRegistry)
@@ -20,12 +20,23 @@ type
     E_Descricao: TEdit;
     E_Abreviatura: TEdit;
     E_Escala: TEdit;
-    E_Porporcao: TEdit;
+    E_Proporcao: TEdit;
     E_MedidaGRupo: TEdit;
-  private
-    { Private declarations }
+  protected
+    procedure ClearAllFields; Override;
+    procedure CriarVariaveis; Override;
+    procedure FinalizaVariaveis; Override;
+    procedure IniciaVariaveis; Override;
+    procedure ShowData; Override;
+    procedure ShowNoData; Override;
+    procedure Insert; Override;
+    procedure Change; Override;
+    function ValidateDelete():boolean; Override;
+    procedure Delete; Override;
+    function ValidateSave():boolean; Override;
+    procedure Save; Override;
   public
-    { Public declarations }
+    medida : TControllerMedida;
   end;
 
 var
@@ -33,6 +44,117 @@ var
 
 implementation
 
+uses UN_MSG;
+
 {$R *.dfm}
+
+procedure TRegMeasure.Change;
+begin
+  inherited;
+  E_Descricao.SetFocus;
+end;
+
+procedure TRegMeasure.ClearAllFields;
+begin
+  inherited;
+  medida.clear;
+end;
+
+procedure TRegMeasure.CriarVariaveis;
+begin
+  inherited;
+  medida := TControllermedida.create(self);
+end;
+
+procedure TRegMeasure.Delete;
+begin
+  medida.delete;
+  inherited;
+end;
+
+procedure TRegMeasure.FinalizaVariaveis;
+begin
+  inherited;
+  FreeAndNil(medida);
+end;
+
+procedure TRegMeasure.IniciaVariaveis;
+begin
+  if Self.CodigoRegistro > 0 then
+  Begin
+    medida.Registro.Codigo := Self.CodigoRegistro;
+    medida.getbyId;
+  End;
+  inherited;
+end;
+
+procedure TRegMeasure.Insert;
+begin
+  inherited;
+  E_Descricao.SetFocus;
+end;
+
+procedure TRegMeasure.Save;
+begin
+  with medida do
+  Begin
+    Registro.Descricao := E_Descricao.Text;
+    Registro.Abreviatura := E_Abreviatura.Text;
+    Registro.Escala := StrToFloatDef(E_Escala.Text, 0);
+    Registro.Proporcao := StrToFloatDef(E_Proporcao.Text, 0);
+    Registro.MedidaCardapio := E_MedidaGrupo.Text;
+
+    salva;
+  End;
+  CodigoRegistro := medida.Registro.Codigo;
+  inherited;
+end;
+
+procedure TRegMeasure.ShowData;
+begin
+  with medida do
+  Begin
+    E_Codigo.Text := Registro.Codigo.ToString;
+    E_Descricao.Text := Registro.Descricao;
+    E_Abreviatura.Text := Registro.Abreviatura;
+    E_Escala.Text := FloatToStrF(Registro.Escala, ffFixed, 10, 2);
+    E_Proporcao.Text := FloatToStrF(Registro.Proporcao, ffFixed, 10, 2);
+    E_MedidaGRupo.Text := Registro.MedidaCardapio;
+  End;
+  inherited;
+end;
+
+procedure TRegMeasure.ShowNoData;
+begin
+  E_Codigo.clear;
+  E_Descricao.clear;
+end;
+
+function TRegMeasure.ValidateDelete: boolean;
+begin
+  Result := True;
+  inherited;
+end;
+
+function TRegMeasure.ValidateSave: boolean;
+begin
+  Result := True;
+
+  if Trim(E_Descricao.Text) = EmptyStr then
+  begin
+    TMsgSetes.ValidaPreenchimentoCampo(L_Descricao.Caption);
+    Result := False;
+    E_Descricao.SetFocus;
+    Exit;
+  end;
+
+  if Trim(E_Abreviatura.Text) = EmptyStr then
+  begin
+    TMsgSetes.ValidaPreenchimentoCampo(L_Abreviatura.Caption);
+    Result := False;
+    E_Abreviatura.SetFocus;
+    Exit;
+  end;
+end;
 
 end.
--- view/module/operation/register/sea_measure.pas
+++ view/module/operation/register/sea_measure.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, base_search, Data.DB, Datasnap.DBClient,
-  Vcl.Menus, Vcl.Buttons, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls, Vcl.ExtCtrls;
+  Vcl.Menus, Vcl.Buttons, Vcl.Grids, Vcl.DBGrids, Vcl.StdCtrls, Vcl.ExtCtrls, ControllerMedida;
 
 type
   TSeaMeasure = class(TBaseSearch)
@@ -14,10 +14,17 @@ type
     L_BuscaCodigo: TLabel;
     E_BuscaDescricao: TEdit;
     E_BuscaCodigo: TEdit;
-  private
-    { Private declarations }
+    cds_searchcodigo: TIntegerField;
+    cds_searchdescricao: TStringField;
+  protected
+    procedure openRegister(pCodigo: Integer);Override;
+    procedure CriarVariaveis; override;
+    procedure FinalizaVariaveis; override;
+    procedure Search; override;
+    procedure GetView; override;
+    procedure SetRegister; override;
   public
-    { Public declarations }
+    medida : TControllerMedida;
   end;
 
 var
@@ -25,6 +32,71 @@ var
 
 implementation
 
+uses reg_measure;
+
 {$R *.dfm}
+
+procedure TSeaMeasure.CriarVariaveis;
+begin
+  inherited;
+  medida := TControllerMedida.create(self);
+end;
+
+procedure TSeaMeasure.FinalizaVariaveis;
+begin
+  inherited;
+  FreeAndNil(medida);
+end;
+
+procedure TSeaMeasure.GetView;
+begin
+  openRegister(cds_searchCodigo.AsInteger);
+end;
+
+procedure TSeaMeasure.openRegister(pCodigo: Integer);
+var
+  Lc_form : TRegMeasure;
+begin
+  Lc_form := TRegMeasure.Create(self);
+  Try
+    Lc_form.CodigoRegistro := pCodigo;
+    Lc_form.ShowModal;
+  Finally
+    FreeAndNil(Lc_form);
+  End;
+end;
+
+procedure TSeaMeasure.Search;
+var
+  i: Integer;
+begin
+  medida.Clear;
+
+  medida.Parametros.FieldName.Codigo := StrToIntDef(E_BuscaCodigo.Text, 0);
+  medida.Parametros.FieldName.Descricao := E_BuscaDescricao.Text;
+
+  medida.Search;
+
+  if not cds_search.Active then
+    cds_search.CreateDataSet;
+
+  cds_search.EmptyDataSet;
+
+  cds_search.DisableControls;
+
+  for i := 0 to Pred(medida.Lista.Count) do
+    cds_search.AppendRecord([medida.Lista[i].Codigo, medida.Lista[i].Descricao]);
+
+  cds_search.EnableControls;
+
+  inherited;
+end;
+
+procedure TSeaMeasure.SetRegister;
+begin
+  openRegister(0);
+end;
+
+end.
 
 end.
